5b85fb715e41602f983116d0cd04c41b6232db2b
Madhur <ahuja.madhur@gmail.com>
2023-05-27 14:07:55 +0530

    feat: new changes

--- backup.sh
+++ backup.sh
@@ -4,16 +4,16 @@ dfolder=$HOME/github/personal/dotfiles/home/madhur
 guake --save-preferences ~/github/personal/dotfiles/home/madhur/myguakeprefs
 
 cp ~/.config/conky/* $dfolder/.config/conky/
-cp -r ~/.config/dunst $dfolder/.config/
-cp -r ~/.config/gsimplecal $dfolder/.config/
+#cp -r ~/.config/dunst $dfolder/.config/
+#cp -r ~/.config/gsimplecal $dfolder/.config/
 cp -r ~/.config/gtk-{2,3,4}.0 $dfolder/.config/
-cp -r ~/.config/i3 $dfolder/.config/
-cp -r ~/.config/i3status-rust $dfolder/.config/
+#cp -r ~/.config/i3 $dfolder/.config/
+#cp -r ~/.config/i3status-rust $dfolder/.config/
 cp  ~/.config/jgmenu/* $dfolder/.config/jgmenu/
 cp ~/.config/kitty/* $dfolder/.config/kitty/
 cp -r ~/.config/neofetch $dfolder/.config/
 cp -r ~/.config/picom $dfolder/.config/
-cp -r ~/.config/polybar $dfolder/.config/
+#cp -r ~/.config/polybar $dfolder/.config/
 #cp -r ~/.config/xmonad $dfolder/.config/
 rsync -avh --delete --exclude='.git/' ~/.config/xmonad $dfolder/.config/
 #cp -r ~/.config/awesome $dfolder/.config/
@@ -25,7 +25,7 @@ cp ~/.config/rofi/* $dfolder/.config/rofi/
 cp -r ~/.config/sxhkd $dfolder/.config/
 cp -r ~/.config/starship.toml $dfolder/.config/
 cp -r ~/.config/redshift.conf $dfolder/.config/
-cp -r ~/.config/betterlockscreenrc $dfolder/.config/
+#cp -r ~/.config/betterlockscreenrc $dfolder/.config/
 cp -r ~/.cache/wal $dfolder/.cache/
 cp -r ~/.config/mimeapps.list $dfolder/.config/
 
@@ -47,10 +47,15 @@ cp -r ~/.tmux.conf $dfolder/
 cp -r ~/Pictures/wallpapers $dfolder/Pictures/
 cp -r ~/.gitconfig $dfolder/
 cp -r ~/.gtkrc-2.0 $dfolder/
-cp ~/.stalonetrayrc $dfolder/
+#cp ~/.stalonetrayrc $dfolder/
 cp ~/.dir_colors $dfolder/
 cp ~/.aliases $dfolder/
 cp ~/.functions $dfolder/
 
 
 sudo cp /root/.bashrc $HOME/github/personal/dotfiles/root/
+cp /etc/pacman.conf ./etc/pacman.conf
+cp /etc/my.cnf ./etc/my.cnf
+cp /etc/fstab ./etc/fstab
+
+pacman -Q > pacman.txt
