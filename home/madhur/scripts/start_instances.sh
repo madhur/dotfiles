b78291488fca1b416aa6ca991bcb5fc1de66feff
Madhur Ahuja <ahuja.madhur@gmail.com>
2024-02-24 23:13:28 +0530

    feat: backmerge

---
+++ home/madhur/scripts/start_instances.sh
@@ -0,0 +1,1 @@
+aws ec2 start-instances --instance-ids i-06ec57184ffcdac48 i-01b8c40caea65ad68 i-06f3837ce59c182b4 i-07445b5bb8a03dc2e i-0d4e276228ec06d0b --region ap-south-1
---
+++ home/madhur/scripts/start_vpn.sh
@@ -0,0 +1,1 @@
+sudo openvpn /etc/openvpn/client/client.conf
