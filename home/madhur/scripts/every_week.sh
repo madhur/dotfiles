bb32821a807b370a7c9ac142126bf3d0ef190a8a
Madhur Ahuja <ahuja.madhur@gmail.com>
2025-09-08 09:18:38 +0530

    backup: Dotfiles updated on 8/9/2025, 9:18:37 am IST

--- home/madhur/scripts/every_week.sh
+++ home/madhur/scripts/every_week.sh
@@ -2,3 +2,5 @@
 #find /home/madhur/.cache/ -type f -atime +30 -print -delete
 #docker system prune -af --volumes
 cd /home/madhur/Desktop/python/email_reader && /home/madhur/.virtualenvs/python-rsha/bin/python /home/madhur/Desktop/python/email_reader/gmail_reader.py
+
+cd /home/madhur/gitpersonal/dotfiles && node_modules/gulp-cli/bin/gulp.js backup-and-push
