f095d4a60952207fed1e9ade6dfc7b7db4693772
Madhur Ahuja <ahuja.madhur@gmail.com>
2025-11-15 00:47:25 +0530

    backup: Dotfiles updated on 15/11/2025, 12:47:18 am IST

--- home/madhur/.config/awesome/keybindings/clientkeys.lua
+++ home/madhur/.config/awesome/keybindings/clientkeys.lua
@@ -105,4 +105,36 @@ end, {
     group = "client"
 }))
 
+awful.key({modkey}, "w", function()
+    if cl_menu then
+        cl_menu:hide()
+        cl_menu = nil
+    else
+        client_list = {}
+        local tag = awful.tag.selected()
+        for i = 1, #tag:clients() do
+            cl = tag:clients()[i]
+            if tag:clients()[i].minimized then
+                prefix = "_ "
+            else
+                prefix = "* "
+            end
+            if not awful.rules.match(cl, {
+                class = "Conky"
+            }) then
+                client_list[i] = {prefix .. cl.name, function()
+                    tag:clients()[i].minimized = not tag:clients()[i].minimized
+                end, cl.icon}
+            end
+        end
+        cl_menu = awful.menu({
+            items = client_list,
+            theme = {
+                width = 300
+            }
+        })
+        cl_menu:show()
+    end
+end)
+
 return clientkeys
---
+++ home/madhur/scripts/start_temporal_worker.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+cd /home/madhur/Desktop/python && /home/madhur/.virtualenvs/python-rsha/bin/python temporal-worker.py worker
